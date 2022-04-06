b6db5ce2d286bc15186cbd829a6ef33a482c8d71
kubilayturgut <kubilay.turgut@gmail.com>
2022-04-06 16:14:34 +0900

    aws deploy v1

--- reactool/settings.py
+++ reactool/settings.py
@@ -177,15 +177,40 @@ if DEBUG:
   # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
   # MEDIA_URL = '/media/'
 
-  DJANGO_VITE_ASSETS_PATH = BASE_DIR / "app/dist"
-  STATIC_URL = '/static/'
-  STATIC_ROOT = BASE_DIR / 'static'
+  # DJANGO_VITE_ASSETS_PATH = BASE_DIR / "app/dist"
+  # STATIC_URL = '/static/'
+  # STATIC_ROOT = BASE_DIR / 'static'
+  # STATICFILES_DIRS = [
+  #   DJANGO_VITE_ASSETS_PATH,
+  # ]
+  # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+  # MEDIA_URL = '/media/'
+
+  AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+  AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+  AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+  AWS_DEFAULT_ACL = 'public-read'
+  AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+  AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+
+  # s3 static settings
+  AWS_S3_FILE_OVERWRITE = False
+  AWS_LOCATION = 'static'
+  STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+  STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+  # s3 public media settings
+  PUBLIC_MEDIA_LOCATION = 'media'
+  MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+  DEFAULT_FILE_STORAGE = "reactool.aws_storages.MediaStorage"
+
+  # STATIC_ROOT = BASE_DIR / 'static'
+  DJANGO_VITE_ASSETS_PATH = BASE_DIR / "static"
+  # STATIC_ROOT = 'static'
+  # DJANGO_VITE_ASSETS_PATH = "static"
   STATICFILES_DIRS = [
     DJANGO_VITE_ASSETS_PATH,
   ]
-  MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-  MEDIA_URL = '/media/'
-
 else:
   # Where ViteJS assets are built.   # aws settings
   AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
@@ -206,7 +231,7 @@ else:
   MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
   DEFAULT_FILE_STORAGE = "reactool.aws_storages.MediaStorage"
 
-  STATIC_ROOT = BASE_DIR / 'static'
+  # STATIC_ROOT = BASE_DIR / 'static'
   DJANGO_VITE_ASSETS_PATH = BASE_DIR / "static"
   # STATIC_ROOT = 'static'
   # DJANGO_VITE_ASSETS_PATH = "static"
