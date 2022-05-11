6cd45dd5a1b2ef13dc3722c096803fca6bffa9fc
kubilayturgut <kubilay.turgut@gmail.com>
2022-05-11 09:56:30 +0900

    dev update

--- common/helpers.py
+++ common/helpers.py
@@ -1,5 +1,5 @@
 import requests
-# import cv2
+import cv2
 import mojimoji
 
 from decouple import config
@@ -41,22 +41,22 @@ def get_image_px_sizes(image_path):
     'channel': None
   }
 
-  # try:
-  #   # print(f'https://{AWS_S3_CUSTOM_DOMAIN}/media/' + str(image_path), "kubilay")
-  #   img = Image.open(requests.get(f'https://{AWS_S3_CUSTOM_DOMAIN}/media/' + str(image_path), stream=True).raw)
-  #
-  #   if DEBUG:
-  #     img = Image.open(requests.get("http://127.0.0.1:8000/media/" + str(image_path), stream=True).raw)
-  #
-  #   # print(img.size, "image size")
-  #
-  #   w, h = img.size
-  #   context = {
-  #     'width': w,
-  #     'height': h,
-  #   }
-  # except cv2.error as e:
-  #   print(e, "cv error")
+  try:
+    # print(f'https://{AWS_S3_CUSTOM_DOMAIN}/media/' + str(image_path), "kubilay")
+    img = Image.open(requests.get(f'https://{AWS_S3_CUSTOM_DOMAIN}/media/' + str(image_path), stream=True).raw)
+
+    if DEBUG:
+      img = Image.open(requests.get("http://127.0.0.1:8000/media/" + str(image_path), stream=True).raw)
+
+    # print(img.size, "image size")
+
+    w, h = img.size
+    context = {
+      'width': w,
+      'height': h,
+    }
+  except cv2.error as e:
+    print(e, "cv error")
 
   return context
 
@@ -66,41 +66,41 @@ def calculate_ppm(image_path):
     "ppm": None,
     "shape": None
   }
-  # if image_path is not None:
-  #   processed_data = None
-  #
-  #   try:
-  #     img = Image.open(image_path)
-  #     processed_data = pytesseract.image_to_string(img, config='-c tessedit_do_invert=0 --psm 11')
-  #     context["shape"] = get_image_px_sizes(image_path)
-  #   except ValueError:
-  #     pass
-  #
-  #   if processed_data:
-  #     num_array = []
-  #     for item in processed_data:
-  #       string = ""
-  #       for deform_num in mojimoji.zen_to_han(item):
-  #         num = get_normal_number(deform_num)
-  #         if int(num.isdigit()) or num == "," or num == ".":
-  #           string += num
-  #
-  #         num_array.append(string)
-  #
-  #     string = ""
-  #     for i in num_array:
-  #       if "" == i:
-  #         i = "S"
-  #
-  #       string += i
-  #
-  #     data_arr = string.split("S")
-  #
-  #     extract_num = None
-  #     for i in data_arr:
-  #       if 2 < len(i) < 8:
-  #         extract_num = i
-  #
-  #     context["ppm"] = str(context["shape"]["width"] / (int(extract_num.replace(",","")) / 1000))[:6]
+  if image_path is not None:
+    processed_data = None
+
+    try:
+      img = Image.open(image_path)
+      processed_data = pytesseract.image_to_string(img, config='-c tessedit_do_invert=0 --psm 11')
+      context["shape"] = get_image_px_sizes(image_path)
+    except ValueError:
+      pass
+
+    if processed_data:
+      num_array = []
+      for item in processed_data:
+        string = ""
+        for deform_num in mojimoji.zen_to_han(item):
+          num = get_normal_number(deform_num)
+          if int(num.isdigit()) or num == "," or num == ".":
+            string += num
+
+          num_array.append(string)
+
+      string = ""
+      for i in num_array:
+        if "" == i:
+          i = "S"
+
+        string += i
+
+      data_arr = string.split("S")
+
+      extract_num = None
+      for i in data_arr:
+        if 2 < len(i) < 8:
+          extract_num = i
+
+      context["ppm"] = str(context["shape"]["width"] / (int(extract_num.replace(",","")) / 1000))[:6]
 
   return context
