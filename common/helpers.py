c8c0bc9331d3260d702624bfba3e2e8bb4e219d6
kubilayturgut <kubilay.turgut@gmail.com>
2022-05-13 10:22:18 +0900

    dev update

--- common/helpers.py
+++ common/helpers.py
@@ -42,19 +42,18 @@ def get_image_px_sizes(image_path):
   }
 
   try:
-    # # print(f'https://{AWS_S3_CUSTOM_DOMAIN}/media/' + str(image_path), "kubilay")
+    print(f'https://{AWS_S3_CUSTOM_DOMAIN}/media/' + str(image_path), "kubilay")
     img = Image.open(requests.get(f'https://{AWS_S3_CUSTOM_DOMAIN}/media/' + str(image_path), stream=True).raw)
-
+    print("kubilay")
     if DEBUG:
       img = Image.open(requests.get("http://127.0.0.1:8000/media/" + str(image_path), stream=True).raw)
-
-    # print(img.size, "image size")
 
     w, h = img.size
     context = {
       'width': w,
       'height': h,
     }
+
   except cv2.error as e:
     print(e, "cv error")
 
