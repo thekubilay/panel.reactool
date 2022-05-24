02aa509508a0c4452478ce2e54580e6459856f71
kubilayturgut <kubilay.turgut@gmail.com>
2022-05-24 15:18:01 +0900

    prod update

--- news/routes/views.py
+++ news/routes/views.py
@@ -12,6 +12,23 @@ class NewsLetterViewSet(viewsets.ModelViewSet):
   serializer_class = NewsLetterSerializer
   permission_classes = [IsSuperAdmin | IsAdmin | IsAuthenticated]
 
+  def get_permissions(self):
+    permission_classes = [IsAuthenticated]
+    if self.action == "create":
+      permission_classes = [IsSuperAdmin | IsAdmin]
+    elif self.action == "update":
+      permission_classes = [IsSuperAdmin | IsAdmin]
+    elif self.action == "partial_update":
+      permission_classes = [IsSuperAdmin | IsAdmin]
+    elif self.action == "destroy":
+      permission_classes = [IsSuperAdmin | IsAdmin]
+    elif self.action == "list":
+      permission_classes = [IsSuperAdmin | IsAdmin]
+    elif self.action == "retrieve":
+      permission_classes = [IsSuperAdmin]
+
+    return [permission() for permission in permission_classes]
+
   def create(self, request, *args, **kwargs):
     serializer = NewsLetterSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
